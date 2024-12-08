2e5f32f0c55cfd0e8142ba4ca52da4b6f37be90f
Macktireh <abdimack97@gmail.com>
2024-12-08 18:12:56 +0100

    migrations: Add pgcrypto extension to the database if it does not exist.

---
+++ src/main/resources/db/changelog/migrations/0003_create_table_users_and_user_roles.sql
@@ -0,0 +1,24 @@
+/*
+DATE: 2024-12-08 13:40:14
+DESCRIPTION: Create the users table and user_roles table
+*/
+
+
+CREATE TABLE IF NOT EXISTS users 
+(
+    id BIGSERIAL PRIMARY KEY,
+    public_id UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS user_roles 
+(
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    role_id BIGINT NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id) 
+);
